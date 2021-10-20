bdc285be3f0a4a9e5ee399b75a6a3962ee7e3679
mohamad.barek <mohamad.barek@ideatolife.me>
2021-10-20 11:24:40 +0300

    initial commit

---
+++ CreatingPod/CreatingPod.h
@@ -0,0 +1,18 @@
+//
+//  CreatingPod.h
+//  CreatingPod
+//
+//  Created by mohamad barek on 10/20/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CreatingPod.
+FOUNDATION_EXPORT double CreatingPodVersionNumber;
+
+//! Project version string for CreatingPod.
+FOUNDATION_EXPORT const unsigned char CreatingPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreatingPod/PublicHeader.h>
+
+
